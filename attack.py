e0c94344b15d9648a3aade5d300caf45c17942cc
markinfante <minfante@vassar.edu>
2021-04-11 16:21:53 -0400

    Beating it down!

--- attack.py
+++ attack.py
@@ -37,6 +37,7 @@ def attack(ciphertext):
     # initialize fakeciphertext and plaintext to 16 char lists
     fakeciphertext = ['0'] * block_length   # c1'
     plaintext = ['0'] * block_length  # p2
+    fakeplaintext = [chr(block_length-i) for i in range(block_length)]
     
     # loop through each character front to back
     for i, c in enumerate(fakeciphertext):
@@ -48,17 +49,20 @@ def attack(ciphertext):
             # if it returns true, continue decryption
             if query_oracle(fakeciphertext, c2):
                 # A) C1' xor P2' 
-                i2 = ord(fakeciphertext[index]) ^ (i + 1)
+                i2 = ord(fakeciphertext[index]) ^ ord(fakeplaintext[index])
                 # B) I2 xor C1 
-                plaintext[index] = chr(i2 ^ ord(c1[index])) 
+                plaintext[index] = chr(i2 ^ ord(c1[index]))
                 # C) This is unfinished
                 k = index
                 while k < block_length:
                     # i2 = C1[k] ^ P2[k]
                     i2 = ord(c1[k]) ^ ord(plaintext[k])
                     # update fakeciphertext = i2 ^ P2'
-                    fakeciphertext[k] = chr(i2 ^ (i + 1)) 
+                    fakeciphertext[k] = chr(i2 ^ ord(fakeplaintext[k]) + 1) 
                     k += 1
+
+                # break to not write over our data in fakeciphertext[index]
+                break
 
     # return our plaintext 
     return plaintext 
@@ -67,7 +71,7 @@ def attack(ciphertext):
 
 # test attack by encrypting a message and then calling attack method
 def test_attack():
-
+    #[edibly 'a','w','e','s','o','m','e','\x03','\x03','\x03']
     message = "Sloths are incredibly awesome"
     output = attack(encrypt(message))
     print(output)
