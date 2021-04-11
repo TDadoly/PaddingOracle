6163fa1e3af932c48dfcf454adeda3f1dbe934de
TDadoly <tdadoly@gmail.com>
2021-04-10 20:33:48 -0400

    Add files via upload

---
+++ attack.py
@@ -0,0 +1,46 @@
+#!usr/bin/env python3
+
+from oracle import encrypt, oracle, BLOCK_SIZE
+
+# utility method to split real ciphertext into a list of blocks
+def blockify(ciphertext):
+
+    # calculate number of blocks
+    num_blocks = len(ciphertext)/16
+
+    # make list of empty lists
+    blocks = [[]] * num_blocks
+
+    # break ciphertext into blocks
+    for i in range(num_blocks):
+        blocks[i] = ciphertext[i * BLOCK_SIZE: (i + 1) * BLOCK_SIZE]
+
+    return blocks
+
+# utlity method to query oracle
+# takes fakeciphertext block and real ciphertext block and combines them
+# queries oracle, returns true if oracle returns true
+def query_oracle(fake, realblock):
+
+    # make a string that contains the fakeciphertext and the real ciphertext block
+    # this is C1'C2
+    # this is a string with 32 characters
+    totalciphertext = ''.join(fake) + realblock
+
+    return(oracle(totalciphertext))
+
+def attack(ciphertext):
+
+    # YOUR CODE GOES HERE
+
+
+# test attack by encrypting a message and then calling attack method
+def test_attack():
+
+    message = "Sloths are incredibly awesome"
+    output = attack(encrypt(message))
+    print(output)
+    
+if __name__ == '__main__':
+    test_attack()
+
